a02489e50def7f2dd7922752492eb8eee40de6de
Rudrika <rudrikasingh1856@gmail.com>
2024-08-06 09:17:53 +0530

    n

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../navbar/Navbar'
 
 const Landing = () => {
   return (
-
-    <div>Landing</div>
+    <>  <Navbar />
+    <div>Landing</div></>
+  
   )
 }
 
