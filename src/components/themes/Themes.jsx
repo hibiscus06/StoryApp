48f16cd60d764aac547dabaaad32dd2283c305c4
Rudrika <rudrikasingh1856@gmail.com>
2024-09-02 19:13:22 +0530

    changes

--- src/components/themes/Themes.jsx
+++ src/components/themes/Themes.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Themes = () => {
-  return <div>Themes</div>;
+  return (
+    <>
+      <div className="">
+        {/* Themes */}
+        <div className="border"></div>
+        {/* Prompts */}
+        <div className="border"></div>
+      </div>
+    </>
+  );
 };
 
 export default Themes;
